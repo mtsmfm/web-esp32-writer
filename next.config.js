03e4d282ce85d3457efa0a5b1ecff14aff88c5c0
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2022-06-27 12:11:33 +0000

    Subdir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: "/web-esp32-writer",
+  basePath: "/web-esp32-writer",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
