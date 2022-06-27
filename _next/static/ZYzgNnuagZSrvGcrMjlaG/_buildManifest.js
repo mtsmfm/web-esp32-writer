689aa419d60f9eb5c7388a718bc5d4d782447085
mtsmfm <mtsmfm@gmail.com>
2022-06-27 11:30:53 +0000

    Deploying to gh-pages from @ mtsmfm/web-esp32-writer@1f75719faf8235d5bbebfd309926b63ee040a043 🚀

---
+++ _next/static/ZYzgNnuagZSrvGcrMjlaG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fec483df-6602518012ee82f7.js","static/chunks/137-2a61fa59fda1f2f8.js","static/chunks/pages/index-cf42cf28243e07b0.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
